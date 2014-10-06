3719bdcc18caba7721278d5a9781af63f47b84b9
unknown <bforte@.smoothstone.local>
2014-10-06 08:15:28 -0400

    init techbox

---
+++ techbox/api/service1.php
@@ -0,0 +1,5 @@
+<?php
+if(isset($_POST)){
+	echo("<pre>");print_r($_POST);echo("</pre>");
+}
+?>
---
+++ techbox/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel='stylesheet' type='text/css' href='css/index.css'>
+		<script type='text/javascript' src='js/index.js'></script>
+	</head>
+	<body>
+		<h3>Techbox: </h3>
+		<p>
+			a sandbox for experimenting with technology items
+		</p>
+		<br><a href="zend/ZendSkeletonApplication/public/" target="_blank">zend: php framework</a></br>
+		
+		</br> <a href="site/"  >game: with HTML5 canvas and js</a></br>
+		 
+		</br><a href ="js/backbone/crud/">CRUD with backbone.js and jQuery</a>
+		</br><a href ="css3d/sandbox.php">3D trackball with CSS3D and js</a>
+		<p>Still need:</p>
+		<ul>
+			<li>2D animation</li>
+			<li>3D animation</li>
+		</ul>
+		<p>Have elsewhere</p>
+			<ul>
+				<li>CRUD with indexedDB : estimator app</li>
+			</ul>
+		</p>
+	</body>
+</html>
+<script></script>
